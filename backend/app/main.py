4f9fd78a8d9fdb341569ffdab4689b749038a6f9
Aki Foudila <aki.foudila@fns.fi>
2020-04-27 20:41:01 +0300

    Add read_calendar view

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import schemas, operations
+from .database import Session as SessionLocal
+
 
 app = FastAPI()
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+
+@app.get("/calendar/{url_hash}/", response_model=schemas.Calendar)
+def read_calendar(url_hash: str, db: Session = Depends(get_db)):
+    return operations.get_calendar_by_hash(db, url_hash)
