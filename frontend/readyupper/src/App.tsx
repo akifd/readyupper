38db76d74434198ddc6964b15daa5a64f844f080
Aki Foudila <aki.foudila@fns.fi>
2020-04-29 17:37:36 +0300

    Update frontpage

--- frontend/readyupper/src/App.tsx
+++ frontend/readyupper/src/App.tsx
@@ -25,6 +25,28 @@ function App() {
             <Card>
               <CardContent>
                 <Typography component="h2" variant="h4" gutterBottom>
+                  About
+                </Typography>
+                <Typography component="p" gutterBottom>
+                  With Readyupper you can quickly schedule events/meetings by creating a calendar
+                  and adding datetimes and participants. Share the link and everyone can choose the
+                  datetimes that are available for them.
+                </Typography>
+                <ul>
+                  <li>Free and fast to use</li>
+                  <li>No registration</li>
+                  <li>Anyone can edit calendars</li>
+                </ul>
+                <Typography component="p" gutterBottom>
+                  Calendars are automatically deleted after 3 months.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Card>
+              <CardContent>
+                <Typography component="h2" variant="h4" gutterBottom>
                   Create new calendar
                 </Typography>
                 <TextField label="Calendar name" variant="filled" fullWidth />
@@ -32,22 +54,6 @@ function App() {
               <CardActions>
                 <Button variant="contained" color="primary" fullWidth>
                   Create calendar
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item sm={12} md={6}>
-            <Card>
-              <CardContent>
-                <Typography component="h2" variant="h4" gutterBottom>
-                  Open existing calendar
-                </Typography>
-                <TextField label="Calendar hash" variant="filled" fullWidth />
-              </CardContent>
-              <CardActions>
-                <Button variant="contained" fullWidth>
-                  Open calendar
                 </Button>
               </CardActions>
             </Card>
