0ab2883a9d829bfb5d3a29da68eefcfebb9b0a6e
Aki Foudila <aki.foudila@fns.fi>
2020-05-03 13:14:11 +0300

    Add participant model

--- backend/readyupper/models.py
+++ backend/readyupper/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -9,4 +11,18 @@ class Calendar(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     url_hash = Column(String(32), nullable=False, unique=True)
-    created = Column(DateTime, nullable=False, server_default="NOW()")
+    created = Column(DateTime, nullable=False, server_default=func.now())
+
+    participants = relationship("Participant", order_by=lambda: Participant.id,
+                                back_populates="calendar")
+
+
+class Participant(Base):
+    __tablename__ = "participants"
+
+    id = Column(Integer, primary_key=True)
+    calendar_id = Column(Integer, ForeignKey("calendars.id"), nullable=False)
+    name = Column(String(255), nullable=False)
+    created = Column(DateTime, nullable=False, server_default=func.now())
+
+    calendar = relationship("Calendar", back_populates="participants")
