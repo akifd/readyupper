5942c70f2991f483ae5a72a4d71007a3d5686b13
Aki Foudila <aki.foudila@fns.fi>
2020-07-29 17:35:58 +0300

    Add participants/events table to calendar detail

--- frontend/readyupper/src/components/CalendarDetail.tsx
+++ frontend/readyupper/src/components/CalendarDetail.tsx
@@ -1,13 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Typography from '@material-ui/core/Typography'
 import { Link } from 'react-router-dom'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
 
-import { Calendar } from '../interfaces'
+import { Calendar, Participant, Entry } from '../interfaces'
+import { fetchEntries, fetchParticipants } from '../utils'
+import ErrorMessage from './ErrorMessage'
 
 
 function CalendarDetail(props: { calendar: Calendar }) {
+  let [error, setError] = useState("")
+  let [participants, setParticipants] = useState([])
+  let [entries, setEntries] = useState([])
+
+  // Participant fetching.
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetchParticipants(props.calendar.id)
+
+        if (response.status === 200)
+          setParticipants(response.data)
+      }
+      catch (error) {
+        setError("Participant fetching failed.")
+      }
+    }
+    fetchData()
+  }, [props.calendar.id])
+
+  // Entry fetching.
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetchEntries(props.calendar.id)
+
+        if (response.status === 200)
+          setEntries(response.data)
+      }
+      catch(error) {
+        setError("Entry fetching failed.")
+      }
+    }
+    fetchData()
+  }, [props.calendar.id])
+
+  if (error)
+    return <ErrorMessage message={error} />
+
   return (
     <div>
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Participant</TableCell>
+              {entries.map((entry: Entry) => (
+                <TableCell align="center" key={entry.id}>
+                  {entry.timestamp}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {participants.map((participant: Participant) => (
+              <TableRow>
+                <TableCell component="th" scope="row" key={participant.id}>
+                  {participant.name}
+                </TableCell>
+                {entries.map((entry: Entry) => (
+                  <TableCell align="center" key={entry.id}>
+                    <Checkbox/>
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <br />
+
       <Typography component="p" gutterBottom>
         Copy the link to this page and share it to the participants.
       </Typography>
--- frontend/readyupper/src/interfaces.ts
+++ frontend/readyupper/src/interfaces.ts
@@ -3,3 +3,17 @@ export interface Calendar {
   name: string,
   created: string,
 }
+
+export interface Participant {
+  id: string,
+  calendar_id: string,
+  name: string,
+  created: string
+}
+
+export interface Entry {
+  id: string,
+  calendar_id: string,
+  timestamp: string,
+  created: string
+}
